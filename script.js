459b8f1226a4bf10ee923e97931af4bd265ef45a
Saara <saara.orvas@student.laurea.fi>
2025-12-01 13:55:19 +0200

    Muutettu ajax-kutsu axiosiin, ja DOM-manipulaatio tehdään jQuerylla. Lisätty Google fonts linkit ja muotoiltu ulkoasu.

--- script.js
+++ script.js
@@ -1,67 +1,57 @@
-const hakukentta = document.querySelector(".hakukentta");
-hakukentta.addEventListener("keypress", clickEnter); // Lisätään kuuntelija hakukenttään
-
 // Oletuksena haetaan reseptin nimen mukaan
 let hakutyyppi = "StrMeal"
 
-// Enter-näppäimen painalluksesta submit
-function clickEnter (e) {
-    // Tarkistetaan, että on painettu Enter-näppäintä
-    if (e.key === "Enter") {
-        e.preventDefault(); // Estetään lomakkeen oletustoiminta
-        const pituus = hakukentta.value.length;
-        const hakusana = hakukentta.value.trim();
+// API -haku tapahtuu Enter-näppäintä painamalla
+$(document).ready(function() {
+    $(".hakukentta").keypress(function(e) {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Estetään lomakkeen oletustoiminta
+            const hakusana = $(".hakukentta").val().trim();
 
-        // Hakukentän tarkastus
-        if (pituus === 0) {
-        alert("Hakukenttä on tyhjä.")
-        hakukentta.disabled = true;
-        return;
-    }
-        // Jos kenttä ei ole tyhjä
-        haeData(hakusana);
-    }
-}
-
-// Luodaan AJAX-olio
-function haeData (hakusana) {
-// Luodaan URL, jossa on käyttäjän syöte otettu huomioon
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + encodeURIComponent(hakusana);
-    let xhr = new XMLHttpRequest();
-    // Kerrotaan mihin osoitteeseen tietopyyntö lähtee
-    xhr.open("GET", url, true);
-    //Määritellään käsittelijä vastauksen saapuessa
-    xhr.onreadystatechange = function () {
-        // Jos ei ole tullut virheitä matkalla
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Muutetaan vastaus JSON-muotoon
-            let data = JSON.parse(xhr.responseText);
-            // Tarkistetaan löytyikö yhtään tulosta
-            if (!data.meals) {
-                document.getElementById("tulokset").innerText =
-                "Hakusanalla ei löytynyt yhtään reseptiä. Kokeile toista hakusanaa."
+            // Hakukentän tarkastus
+            if (hakusana === "") {
+                alert("The search bar is empty.")
                 return;
             }
 
-            // Näytetään konsolissa data
-            data.meals.forEach(meal => {
-                console.log(meal.strMealThumb);
-                console.log("Recipe:", meal.strMeal);
-                console.log("Category:", meal.strCategory);
-                console.log("Country:", meal.strArea);
+            // Luodaan URL, jossa on käyttäjän syöte otettu huomioon
+            let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + encodeURIComponent(hakusana);
 
-            })
-        } 
-        // Mikäli on tullut virheitä
-        else if (xhr.status === 403) {
-            document.getElementById("tulokset").innerText =
-            "Haussa tapahtui virhe: palvelin esti haun (403). Tarkista hakutyyppi tai kokeile myöhemmin uudelleen.";
-        }
-        else if (xhr.status === 404) {
-            document.getElementById("tulokset").innerText =
-            "Haussa tapahtui virhe: reseptiä ei löytynyt (404). Kokeile toista hakusanaa.";
-        }
-    };
-    // Lähetetään edellä muovattu AJAX-olio
-    xhr.send();
-}
+            // Axios-pyyntö
+            axios.get(url)
+                .then(function(response) {
+                    let data = response.data;
+                    if (!data.meals) {
+                        $("#tulokset").text("No recipes found. Try another recipe name.");
+                        return;
+                    }
+
+                    // Näytetään hakutulokset div-elementissä
+                    $("#tulokset").empty(); // Poistetaan vanhat hakutulokset
+                    for (let i = 0; i < data.meals.length; i++) {
+                        let meal = data.meals[i];
+                        // Luodaan reseptistä korttinäkymä
+                        $("#tulokset").append(`
+                            <div class = "reseptikortti">
+                                <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}">
+                                <h3>${meal.strMeal}</h2>
+                                <p>Category: ${meal.strCategory}</p>
+                                <p>Region: ${meal.strArea}</p>
+                            </div>
+                        `);
+                    }
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        console.error(error.response.data);
+                        console.error(error.response.status);
+                        console.error(error.response.headers);
+                    } else if (error.request) {
+                        console.error(error.request);
+                    } else {
+                        console.error("Error", error.message);
+                    }
+                    $("#tulokset").text("There was an error while fetching recipes. Please try again later.")
+                });
+    }});
+});
